efc2d4698f839056accafdad68b846c8c1470ced
mohammed.furqan <mohammed.furqan@servicenow.com>
2018-10-12 12:34:51 +0530

    Show sort icons on header label

--- src/client/components/lists/TableHead.jsx
+++ src/client/components/lists/TableHead.jsx
@@ -25,12 +25,10 @@ class HeaderSearch extends React.Component {
 	render() {
 		const { col: { label, id, sortable }, onSearch } = this.props;
 		// TODO: Move onInputChange out of render
-		const onInputChange = (e) => {
-			debugger
-			onSearch({
-				col: id, value: e.target.value
-			});
-		}
+		const onInputChange = (e) => onSearch({
+			col: id, value: e.target.value
+		})
+
 		return (
 			<td className="table-header-cell">
 				{<input type="text" style={{ width: '100%' }} placeholder="Search" onChange={onInputChange} />}
@@ -43,22 +41,21 @@ class HeaderLabel extends React.Component {
 	render() {
 		const { col: { label, id, sortable, sortOrder }, onSort } = this.props;
 		// TODO: Move onInputChange out of render
-		const onLabelClick = (e) => {
-			debugger
-			onSort({
-				col: id, asc: !sortOrder
-			});
-		}
+		const onLabelClick = (e) => onSort({
+			col: id, asc: !sortOrder
+		});
 		return (
 			<td className="table-header-cell">
 				<div className="label-wrap">
 					<a className={sortable ? 'sortable-label' : ''} onClick={onLabelClick}>{ label }</a>
-					{sortable && sortedAsc ? <i className="glyphicon glyphicon-chevron-up"></i> : null}
-					{sortable && sortedDesc ? <i className="glyphicon glyphicon-chevron-down"></i> : null}
+					{ isDefined(sortOrder) && sortOrder ? <i className="glyphicon glyphicon-chevron-up"></i> : null}
+					{ isDefined(sortOrder) && !sortOrder ? <i className="glyphicon glyphicon-chevron-down"></i> : null}
 				</div>
 			</td>
 		);
 	}
 }
+
+const isDefined = (a) => typeof a !== 'undefined' && a !== null;
 
 export default TableHeader;
