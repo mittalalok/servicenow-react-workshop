b3d8348aca325f7b63613e90ccc27cffa8fe9b0e
sathibabu.peddada <sathibabu.peddada@servicenow.com>
2018-11-28 19:02:35 +0530

    Fixed indentation issues

--- src/client/components/presentational/forms/inputChips.js
+++ src/client/components/presentational/forms/inputChips.js
@@ -2,8 +2,8 @@ import ChipInput from 'material-ui-chip-input';
 import React, { Component } from 'react';
 
 export default class Chips extends Component {
-  render(){
-    return <ChipInput defaultValue={['foo', 'bar']} />;
-  }
+    render(){
+        return <ChipInput defaultValue={['foo', 'bar']} />;
+    }
 }
 
--- src/client/components/presentational/forms/label.js
+++ src/client/components/presentational/forms/label.js
@@ -1,15 +1,15 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 class Label extends Component {
-  render(){
-    return <label  className = {this.props.class} htmlFor= {this.props.for} >{this.props.value}</label>;
-  }  
+    render(){
+        return <label  className = {this.props.class} htmlFor= {this.props.for} >{this.props.value}</label>;
+    }  
 }
 
 Label.propTypes = {
-  for: PropTypes.string,
-  value: PropTypes.string,
-  class: PropTypes.string,
+    for: PropTypes.string,
+    value: PropTypes.string,
+    class: PropTypes.string,
 };
 
 export default Label;
